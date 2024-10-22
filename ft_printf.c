d057393237c08d988c3afcaf9d929a5d99636fc9
daniel <daniel-congo2@hotmail.com>
2024-10-22 17:28:56 +0200

    ajout des headers dans ft_printf.h + ajout va_list

--- ft_printf.c
+++ ft_printf.c
@@ -6,12 +6,11 @@
 /*   By: daniel149afonso <daniel149afonso@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 15:28:13 by daniel149af       #+#    #+#             */
-/*   Updated: 2024/10/22 13:58:37 by daniel149af      ###   ########.fr       */
+/*   Updated: 2024/10/22 17:13:49 by daniel149af      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
-#include <unistd.h>
+#include "ft_printf.h"
 
 void	find_format(char *str, size_t i)
 {
@@ -35,13 +34,21 @@ void	find_format(char *str, size_t i)
 	{
 
 	}
+	else if (str[++i] == 'x' || str[++i] == 'X')
+	{
+
+	}
 }
 
-int	ft_printf(const char *str)
+int	ft_printf(const char *str, ...)
 {
 	size_t	i;
+	size_t	len;
+	va_list	args;
 
 	i = 0;
+	len = 0;
+	va_start(args, str);
 	while (str[i])
 	{
 		if (str[i] == '%')
@@ -56,6 +63,14 @@ int	ft_printf(const char *str)
 
 int	main(void)
 {
-	ft_printf("Hello%d", );
+	ft_printf("Hello");
 	return (0);
 }
+/*
+va_list:
+C'est une variable spéciale qui représente la liste des arguments.
+Elle est utilisée pour stocker et gérer les arguments variables au sein de la fonction
+va_start(va_list ap, last):
+Cette macro initialise la variable ap (de type va_list) pour commencer à lire les arguments.
+Le paramètre last représente le dernier argument fixe de la fonction, c'est-à-dire l'argument qui précède les arguments variables.
+*/
---
+++ ft_printf.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: daniel149afonso <daniel149afonso@studen    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/22 16:43:21 by daniel149af       #+#    #+#             */
+/*   Updated: 2024/10/22 17:01:15 by daniel149af      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+#define FT_PRINTF_H
+
+#include <stdarg.h>//macros variables
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#endif
