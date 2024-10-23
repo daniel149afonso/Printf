2c39fca465abba9491293564b3291937f19d8339
Daniel Afonso <daafonso@c1r7s5.42lausanne.ch>
2024-10-23 20:56:57 +0200

    ft_putptr en cours pour hexadecimal

--- ft_putptr.c
+++ ft_putptr.c
@@ -3,16 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putptr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: daniel149afonso <daniel149afonso@studen    +#+  +:+       +#+        */
+/*   By: daafonso <daafonso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 18:57:39 by daniel149af       #+#    #+#             */
-/*   Updated: 2024/10/23 19:23:55 by daniel149af      ###   ########.fr       */
+/*   Updated: 2024/10/23 20:56:17 by daafonso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+int ft_put_ptr();
+
 int	ft_putptr(void *ptr)
 {
+    int len;
 
+    len = 0;
+    len += write(1, "0x", 2);
+    if (ptr == 0)
+        len += write(1, "0", 1);
+    else
+    {
+        ft_put_ptr()
+    }
 }
